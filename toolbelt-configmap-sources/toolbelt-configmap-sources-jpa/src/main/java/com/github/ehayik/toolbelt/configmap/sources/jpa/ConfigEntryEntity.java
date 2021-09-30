428eb87e27c2dbd6c76f32df965199b7009d3f6e
eduardo.eljaiek <eduardo.eljaiek.external@raisin.com>
2021-09-30 15:33:36 +0300

    refactor: improve code coverage

--- toolbelt-configmap-sources/toolbelt-configmap-sources-jpa/src/main/java/com/github/ehayik/toolbelt/configmap/sources/jpa/ConfigEntryEntity.java
+++ toolbelt-configmap-sources/toolbelt-configmap-sources-jpa/src/main/java/com/github/ehayik/toolbelt/configmap/sources/jpa/ConfigEntryEntity.java
@@ -4,14 +4,12 @@ import static lombok.AccessLevel.PACKAGE;
 import static lombok.AccessLevel.PROTECTED;
 
 import java.io.Serializable;
-import java.util.Objects;
 import javax.persistence.*;
 import lombok.*;
-import org.hibernate.Hibernate;
 
 @Entity
-@Getter
-@Setter
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString
 @NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor(access = PACKAGE)
@@ -20,23 +18,11 @@ public class ConfigEntryEntity implements Serializable {
 
     @Id
     @Basic(optional = false)
+    @EqualsAndHashCode.Include
     @Column(name = "ts_configmap_key", nullable = false, length = 500)
     private String key;
 
     @Basic(optional = false)
     @Column(name = "ts_configmap_value", nullable = false, length = 2500)
     private String value;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        ConfigEntryEntity that = (ConfigEntryEntity) o;
-        return Objects.equals(key, that.key);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(key);
-    }
 }
